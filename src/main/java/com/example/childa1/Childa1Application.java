95822cb64ca80ce45ca56f8f12023e458d80d376
Jason Clarke <Jason.M.Clarke@ngc.com>
2021-06-05 18:22:34 -0400

    childa initial commit

---
+++ src/main/java/com/example/childa1/Childa1Application.java
@@ -0,0 +1,13 @@
+package com.example.childa1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Childa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Childa1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/childa1/Childa1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.childa1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Childa1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
